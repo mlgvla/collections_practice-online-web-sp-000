9199d11084c8fa975268f0a8d28b5ca732415677
Monica Gerard <mlgerardvla@gmail.com>
2020-03-14 19:38:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,3 +40,13 @@ end
 def sum_array(array)
   array.inject { |sum, n| sum + n }
 end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element << ""
+    else
+      element << "s"
+    end
+  end
+end
